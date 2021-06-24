7f2b9b95cc12cbb216923f14d8c7550d0b3fc543
kaniol-lck <375520360@qq.com>
2021-06-24 22:14:54 +0800

    port to 1.17.

--- src/main/java/net/kaniol/mixin/ClickableWidgetMixin.java
+++ src/main/java/net/kaniol/mixin/ClickableWidgetMixin.java
@@ -1,15 +1,15 @@
 package net.kaniol.mixin;
 
 import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.client.gui.widget.AbstractButtonWidget;
+import net.minecraft.client.gui.widget.ClickableWidget;
 import com.mojang.blaze3d.systems.RenderSystem;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(AbstractButtonWidget.class)
-public class AbstractButtonWidgetMixin {
+@Mixin(ClickableWidget.class)
+public class ClickableWidgetMixin {
 	@Inject(at = @At("HEAD"), method = "render")
 	public void renderHead(MatrixStack matrices, int mouseX, int mouseY, float delta, CallbackInfo ci) {
 		RenderSystem.enableBlend();
